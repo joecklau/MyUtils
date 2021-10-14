9145e9ab6c4ada7366d3bf279dc79c8c72729bb4
Joe@SF <joe@solutionforest.net>
2021-10-15 02:08:02 +0800

    Add HtmlToText

---
+++ MyUtils.Web/Utils/HtmlToText.cs
@@ -0,0 +1,115 @@
+﻿using HtmlAgilityPack;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MyUtils.Web.Utils
+{
+    public static class HtmlToText
+    {
+        #region Public Methods
+
+        public static string LoadPathToPlainText(this string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return string.Empty;
+            }
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(path);
+
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+        
+        public static string FromHtmlToPlainText(this string html, bool trimAndRemoveDuplicatedLineFeeds = false)
+        {
+            if (string.IsNullOrWhiteSpace(html))
+            {
+                return string.Empty;
+            }
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            string result = sw.ToString();
+
+            if (trimAndRemoveDuplicatedLineFeeds)
+            {
+                Regex regexOfLineFeeds = new Regex("[\r\n]+");
+                result = regexOfLineFeeds.Replace(result, "\n")?.Trim('\n');
+            }
+
+            return result;
+        }
+
+        public static void ConvertTo(HtmlNode node, TextWriter outText)
+        {
+            string html;
+            switch (node.NodeType)
+            {
+                case HtmlNodeType.Comment:
+                    // don't output comments
+                    break;
+
+                case HtmlNodeType.Document:
+                    ConvertContentTo(node, outText);
+                    break;
+
+                case HtmlNodeType.Text:
+                    // script and style must not be output
+                    string parentName = node.ParentNode.Name;
+                    if ((parentName == "script") || (parentName == "style"))
+                        break;
+
+                    // get text
+                    html = ((HtmlTextNode)node).Text;
+
+                    // is it in fact a special closing node output as text?
+                    if (HtmlNode.IsOverlappedClosingElement(html))
+                        break;
+
+                    // check the text is meaningful and not a bunch of whitespaces
+                    if (html.Trim().Length > 0)
+                    {
+                        outText.Write(HtmlEntity.DeEntitize(html));
+                    }
+                    break;
+
+                case HtmlNodeType.Element:
+                    switch (node.Name)
+                    {
+                        case "p":
+                            // treat paragraphs as crlf
+                            outText.Write("\r\n");
+                            break;
+                    }
+
+                    if (node.HasChildNodes)
+                    {
+                        ConvertContentTo(node, outText);
+                    }
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static void ConvertContentTo(HtmlNode node, TextWriter outText)
+        {
+            foreach (HtmlNode subnode in node.ChildNodes)
+            {
+                ConvertTo(subnode, outText);
+            }
+        }
+
+        #endregion
+    }
+}
