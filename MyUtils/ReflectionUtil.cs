b9956e3fd869106b393cbc23620e110f458492a5
Joe <ahjoe86@gmail.com>
2022-01-01 17:15:28 +0800

    Add ReflectionUtil.GetCaller()

---
+++ MyUtils/ReflectionUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MyUtils
+{
+    public static class ReflectionUtil
+    {
+        /// <summary>
+        /// Get caller function's name. See https://stackoverflow.com/a/38098931/4684232
+        /// </summary>
+        /// <param name="caller"></param>
+        /// <returns></returns>
+        public static string GetCaller([CallerMemberName] string caller = null) => caller;
+    }
+}
