d1afa379a846b1df370f463e8662ee7089d75df3
Joe <ahjoe86@gmail.com>
2022-01-01 17:59:35 +0800

    Add LogWithCaller() support

--- MyUtils/LoggerExtensions.cs
+++ MyUtils/LoggerExtensions.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace MyUtils
 {
     public static class LoggerExtensions
     {
+        public static void LogDebugWithCaller(this ILogger logger, Func<string> generateMessage, [CallerMemberName] string caller = null) => logger.LogDebug(() => $"[{caller}] {generateMessage()}");
+
+        public static void LogInformationWithCaller(this ILogger logger, Exception exception, string message = "Information", [CallerMemberName] string caller = null) => logger.LogInformation(exception, $"[{caller}] {message}");
+        public static void LogInformationWithCaller(this ILogger logger, string message, [CallerMemberName] string caller = null) => logger.LogInformation($"[{caller}] {message}");
+
+        public static void LogWarningWithCaller(this ILogger logger, Exception exception, string message = "Warning", [CallerMemberName] string caller = null) => logger.LogWarning(exception, $"[{caller}] {message}");
+        public static void LogWarningWithCaller(this ILogger logger, string message, [CallerMemberName] string caller = null) => logger.LogWarning($"[{caller}] {message}");
+
+        public static void LogErrorWithCaller(this ILogger logger, Exception exception, string message = "Error", [CallerMemberName] string caller = null) => logger.LogError(exception, $"[{caller}] {message}");
+        public static void LogErrorWithCaller(this ILogger logger, string message, [CallerMemberName] string caller = null) => logger.LogError($"[{caller}] {message}");
+
+
         /// <summary>
         /// Log the message only-if LogLevel accept <see cref="LogLevel.Debug"/>.
         /// It may help on skipping processing on extra debug message on Production Environment if LogLevel is at-least <see cref="LogLevel.Information"/>
@@ -19,18 +32,18 @@ namespace MyUtils
                 logger.LogDebug(generateMessage());
         }
 
-        /// <summary>
-        /// Log the message only-if LogLevel accept <see cref="LogLevel.Debug"/>.
-        /// It may help on skipping processing on extra debug message on Production Environment if LogLevel is at-least <see cref="LogLevel.Information"/>
-        /// </summary>
-        /// <param name="logger"></param>
-        /// <param name="generateMessage"></param>
-        public static void LogDebug<T>(this ILogger<T> logger, Func<string> generateMessage)
-        {
-            if (logger.IsEnabled(LogLevel.Debug))
-            {
-                logger.LogDebug(generateMessage());
-            }
-        }
+        ///// <summary>
+        ///// Log the message only-if LogLevel accept <see cref="LogLevel.Debug"/>.
+        ///// It may help on skipping processing on extra debug message on Production Environment if LogLevel is at-least <see cref="LogLevel.Information"/>
+        ///// </summary>
+        ///// <param name="logger"></param>
+        ///// <param name="generateMessage"></param>
+        //public static void LogDebug<T>(this ILogger<T> logger, Func<string> generateMessage)
+        //{
+        //    if (logger.IsEnabled(LogLevel.Debug))
+        //    {
+        //        logger.LogDebug(generateMessage());
+        //    }
+        //}
     }
 }
