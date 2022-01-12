4e1e035506b557861bd5808c2b11dd930a82e389
Joe <ahjoe86@gmail.com>
2022-01-12 21:36:18 +0800

    Add Common

---
+++ MyUtils/Common.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace MyUtils
+{
+    public class Common
+    {
+        public static string RuntimeDir => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+    }
+}
