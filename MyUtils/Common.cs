5cb911c8e32b6198d4e24f6e400179a0d48c5f07
Joe <ahjoe86@gmail.com>
2022-06-14 23:55:07 +0800

    Add System.AppContext.BaseDirectory as RuntimeDir option

--- MyUtils/Common.cs
+++ MyUtils/Common.cs
@@ -7,8 +7,9 @@ namespace MyUtils
     public class Common
     {
         //public static string RuntimeDir => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);   // It returns something else for WorkerService project, e.g. C:\\Program Files\\dotnet\\shared\\Microsoft.NETCore.App\\6.0.5\\System.Private.CoreLib.dll
-        //public static string RuntimeDir => Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);   // null for Linux
-        public static string RuntimeDir => Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+        //public static string RuntimeDir => Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);   // Assembly.Location can be null for Linux or SinglePackage. Use System.AppContext.BaseDirectory instead
+        public static string RuntimeDirAlternative => Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+        public static string RuntimeDir => System.AppContext.BaseDirectory;
 
     }
 }
