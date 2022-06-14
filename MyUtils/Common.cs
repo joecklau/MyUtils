5f6042a5d8ff498d9a3b7551b34de27089d5a93d
Joe <ahjoe86@gmail.com>
2022-06-14 18:15:55 +0800

    Add cross-platform support

--- MyUtils/Common.cs
+++ MyUtils/Common.cs
@@ -1,11 +1,14 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
 namespace MyUtils
 {
     public class Common
     {
-        public static string RuntimeDir => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        //public static string RuntimeDir => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);   // It returns something else for WorkerService project, e.g. C:\\Program Files\\dotnet\\shared\\Microsoft.NETCore.App\\6.0.5\\System.Private.CoreLib.dll
+        //public static string RuntimeDir => Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);   // null for Linux
+        public static string RuntimeDir => Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
 
     }
 }
