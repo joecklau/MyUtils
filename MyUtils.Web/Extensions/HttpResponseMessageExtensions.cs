eca2aea91feeee5b56105ef72b46f217a5928739
Joe <ahjoe86@gmail.com>
2022-08-06 01:29:56 +0800

    optional logger

--- MyUtils.Web/Extensions/HttpResponseMessageExtensions.cs
+++ MyUtils.Web/Extensions/HttpResponseMessageExtensions.cs
@@ -19,11 +19,11 @@ namespace MyUtils.Web.Extensions
                 try
                 {
                     var contentText = httpResponseMessage.Content.ReadAsStringAsync().Result;
-                    logger.LogError($"{httpResponseMessage.RequestMessage.Method} {httpResponseMessage.RequestMessage.RequestUri} return {httpResponseMessage.StatusCode} with content: {contentText}");
+                    logger?.LogError($"{httpResponseMessage.RequestMessage.Method} {httpResponseMessage.RequestMessage.RequestUri} return {httpResponseMessage.StatusCode} with content: {contentText}");
                 }
                 catch(Exception ex)
                 {
-                    logger.LogError(ex, $"Fail to read response text for {httpResponseMessage.RequestMessage.Method} {httpResponseMessage.RequestMessage.RequestUri}");
+                    logger?.LogError(ex, $"Fail to read response text for {httpResponseMessage.RequestMessage.Method} {httpResponseMessage.RequestMessage.RequestUri}");
                 }
 
                 throw;
