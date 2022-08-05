6c582e2182bcc7d6c3529afbe62ec3c447058c41
Joe <ahjoe86@gmail.com>
2022-08-05 18:12:24 +0800

    Add HttpResponseMessageExtensions

---
+++ MyUtils.Web/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MyUtils.Web.Extensions
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static HttpResponseMessage EnsureSuccessStatusCodeWithContentLog(this HttpResponseMessage httpResponseMessage, ILogger logger)
+        {
+            try
+            {
+                return httpResponseMessage.EnsureSuccessStatusCode();
+            }
+            catch
+            {
+                try
+                {
+                    var contentText = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                    logger.LogError($"{httpResponseMessage.RequestMessage.Method} {httpResponseMessage.RequestMessage.RequestUri} return {httpResponseMessage.StatusCode} with content: {contentText}");
+                }
+                catch(Exception ex)
+                {
+                    logger.LogError(ex, $"Fail to read response text for {httpResponseMessage.RequestMessage.Method} {httpResponseMessage.RequestMessage.RequestUri}");
+                }
+
+                throw;
+            }
+        }
+    }
+}
