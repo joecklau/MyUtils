bdff5ce47d99c03354340252f1edf2a7b9803cf1
Joe <ahjoe86@gmail.com>
2022-08-06 11:34:45 +0800

    RetryPolicyHelper for http

---
+++ MyUtils/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Extensions.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtils.Extensions
+{
+    public static class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        /// For Query (ReadOnly-method) which is ok to retry
+        /// See https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/implement-http-call-retries-exponential-backoff-polly
+        /// </summary>
+        /// <remarks>
+        /// <b>DO NOT</b> apply on non-ReadOnly method
+        /// </remarks>
+        /// <returns></returns>
+        public static Task<HttpResponseMessage> RetryForTransientHttpErrorOnQuery(this Task<HttpResponseMessage> getTask,
+            ILogger logger = null,
+            System.Threading.CancellationToken cancellationToken = default)
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+
+                ////System.Net.Http.HttpRequestException: A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond. (demo - api.ig.com:443)
+                //// --->System.Net.Sockets.SocketException(10060): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+                //.OrInner<System.Net.Sockets.SocketException>()
+
+                //System.Threading.Tasks.TaskCanceledException: The request was canceled due to the configured HttpClient.Timeout of 100 seconds elapsing.
+                // ---> System.TimeoutException: The operation was canceled.
+                // ---> System.Threading.Tasks.TaskCanceledException: The operation was canceled.
+                // ---> System.IO.IOException: Unable to read data from the transport connection: The I/O operation has been aborted because of either a thread exit or an application request..
+                // ---> System.Net.Sockets.SocketException (995): The I/O operation has been aborted because of either a thread exit or an application request.
+                .OrInner<TimeoutException>()
+
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (result, wait, attempt, ctx) =>
+                    {
+                        string waitToRetryMsg = $"Wait {wait.TotalSeconds}s for {attempt}th retry";
+                        if (result.Result != null)
+                        {
+                            var textContent = result.Result.Content.ReadAsStringAsync();
+                            if (result.Exception is null)
+                            {
+                                logger?.LogWarningWithCaller($"{waitToRetryMsg} for the error when {result.Result.RequestMessage.Method} {result.Result.RequestMessage.RequestUri}. StatusCode: {result.Result.StatusCode}. Content: {textContent}");
+                            }
+                            else
+                            {
+                                logger?.LogWarningWithCaller(result.Exception, $"{waitToRetryMsg} for the error when {result.Result.RequestMessage.Method} {result.Result.RequestMessage.RequestUri}. StatusCode: {result.Result.StatusCode}. Content: {textContent}");
+                            }
+                            result.Result.Dispose();
+                            return;
+                        }
+
+                        if (result.Exception is null)
+                        {
+                            logger?.LogWarningWithCaller($"{waitToRetryMsg} for the error.");
+                        }
+                        else
+                        {
+                            logger?.LogWarningWithCaller(result.Exception, $"{waitToRetryMsg} for the error.");
+                        }
+
+                    })
+                .ExecuteAsync(async token =>
+                {
+                    token.ThrowIfCancellationRequested();
+                    var responseMessage = await getTask;
+                    //responseMessage.EnsureSuccessStatusCodeWithContentLog(logger);
+
+                    return responseMessage;
+                }, cancellationToken);
+        }
+
+
+        public static HttpResponseMessage EnsureSuccessStatusCodeWithContentLog(this HttpResponseMessage httpResponseMessage, ILogger logger)
+        {
+            try
+            {
+                return httpResponseMessage.EnsureSuccessStatusCode();
+            }
+            catch
+            {
+                try
+                {
+                    var contentText = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                    logger?.LogError($"{httpResponseMessage.RequestMessage.Method} {httpResponseMessage.RequestMessage.RequestUri} return {httpResponseMessage.StatusCode} with content: {contentText}");
+                }
+                catch(Exception ex)
+                {
+                    logger?.LogError(ex, $"Fail to read response text for {httpResponseMessage.RequestMessage.Method} {httpResponseMessage.RequestMessage.RequestUri}");
+                }
+
+                throw;
+            }
+        }
+    }
+}
---
+++ MyUtils/RetryPolicyHelper.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Logging;
+using MyUtils;
+using Polly;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyUtils
+{
+    public static class RetryPolicyHelper
+    {
+        /// <summary>
+        /// For GET-method which is ok to retry. Specific to handle Exception types throw from <see cref="HttpClient"/>
+        /// </summary>
+        /// <remarks>
+        /// <b>DO NOT</b> apply on non-GET method
+        /// </remarks>
+        /// <returns></returns>
+        [Obsolete("Should be replaced by HttpResponseMessageExtensions.RetryForTransientHttpErrorOnQuery()")]
+        public static Task<T> RetryPolicyForHttpQuery<T>(this Task<T> getTask,
+            ILogger logger = null,
+            string actionMsgToLog = null,
+            System.Threading.CancellationToken cancellationToken = default)
+        {
+            return Polly.Policy<T>
+                //.Handle<Exception>(ex => ex is not TaskCanceledException and not OperationCanceledException and not UnauthorizedAccessException)
+
+                // Refer to HttpPolicyExtensions.HandleTransientHttpError()
+                .Handle<HttpRequestException>()
+
+                ////System.Net.Http.HttpRequestException: A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond. (demo - api.ig.com:443)
+                //// --->System.Net.Sockets.SocketException(10060): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+                //.OrInner<System.Net.Sockets.SocketException>()
+
+                 //System.Threading.Tasks.TaskCanceledException: The request was canceled due to the configured HttpClient.Timeout of 100 seconds elapsing.
+                 // ---> System.TimeoutException: The operation was canceled.
+                 // ---> System.Threading.Tasks.TaskCanceledException: The operation was canceled.
+                 // ---> System.IO.IOException: Unable to read data from the transport connection: The I/O operation has been aborted because of either a thread exit or an application request..
+                 // ---> System.Net.Sockets.SocketException (995): The I/O operation has been aborted because of either a thread exit or an application request.
+                .OrInner<TimeoutException>()
+
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (result, wait, attempt, ctx) =>
+                    {
+                        string waitToRetryMsg = $"Wait {wait.TotalSeconds}s for {attempt}th retry";
+                        if (result.Exception is null)
+                        {
+                            logger?.LogWarningWithCaller($"{waitToRetryMsg} for the error on {actionMsgToLog}.");
+                        }
+                        else
+                        {
+                            logger?.LogWarningWithCaller(result.Exception, $"{waitToRetryMsg} for the error on {actionMsgToLog}.");
+                        }
+
+                    })
+                .ExecuteAsync(async token =>
+                {
+                    token.ThrowIfCancellationRequested();
+                    return await getTask;
+                }, cancellationToken);
+        }
+    }
+}
